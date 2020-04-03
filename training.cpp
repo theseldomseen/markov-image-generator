9150b1997283c865c45ffee504d8d94a75cee859
theseldomseen <minormotionpicturemusic@gmail.com>
2020-04-03 15:22:42 -0400

    generates training data, inut argument determines the names of the input images

---
+++ training.cpp
@@ -0,0 +1,26 @@
+#include <Magick++.h>
+#include <vector>
+#include <string>
+#include <math.h>
+
+using namespace std; 
+using namespace Magick;
+int main(int argc, char **argv)
+{
+	for(int numsets = 0; numsets < 1; numsets++)
+	{
+		InitializeMagick(*argv);
+		Image img(argv[1] + to_string(numsets) + ".png");
+		int width = img.columns();
+		int height = img.rows();
+		int numIterations = log2(min(width, height));
+
+		//generate different resolutions of the image
+		for( int i = 0; i <= numIterations; i++)
+		{
+			img.write("output_level_" + to_string(i) + "-" + to_string(numsets) + ".png");
+			img.scale("50%");
+		}
+	}
+	return 0;
+}
